0dcd5fffdda632eaf8b445661f09b5b66b7d18f9
Highlander1236 <arturartur242003@gmail.com>
2021-12-16 23:50:39 +0300

    Измененный код
    исправил ошибки

--- Code 2/Code 2/Code 2.cpp
+++ Code 2/Code 2/Code 2.cpp
@@ -4,18 +4,21 @@ using namespace std;
 
 int main()
 {
-	double A, B, C;
+	setlocale(LC_ALL, "Rus");
+	double A,B;
 	int m;
 	cout << "Выберите номер операции(1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление): ";
-	cout >> m;
+	cin >> m;
 	cout << "Задайте значения A и B: \n";
 	cin >> A >> B;
 	if (A == 0 || B == 0)
 		cout << "Неверное значение\n";
+	switch (m)
+	{
 	case 1: std::cout << "Результат: " << A + B; break;
 	case 2: std::cout << "Результат: " << A - B; break;
 	case 3: std::cout << "Результат: " << A * B; break;
 	case 4: std::cout << "Результат: " << A / B; break;
-
+	}
 		return 0;
 }
