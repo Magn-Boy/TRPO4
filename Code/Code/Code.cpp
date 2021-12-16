e68887cb291665f1bd0075bb7eb373adf94be095
notNero <95587468+notNero@users.noreply.github.com>
2021-12-16 22:50:20 +0300

    FixedCode
    Пофиксил

--- Code/Code/Code.cpp
+++ Code/Code/Code.cpp
@@ -1,5 +1,4 @@
-﻿#include "pch.h"
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
@@ -13,37 +12,30 @@ int main()
 		cin >> N;
 		if (N < 0) cout << "Введите число N:\n";
 	} while (N < 0);
-	while (N > 0) {
+	while (N >= 1) {
 		c = N % 10;
 		N = N / 10;
 		count++;
-		if (count = 1)
+		if (count == 1)
 			cout << "Единицы: " << c << endl;
-		if (count = 2)
-			cout << "Единицы: " << c << endl;
-		cout << "Десятки: " << c << endl;
-		if (count = 3)
-			cout << "Единицы: " << c << endl;
-		cout << "Десятки: " << c << endl;
-		cout << "Сотни: " << c << endl;
-		if (count = 4)
-			cout << "Единицы: " << c << endl;
-		cout << "Десятки: " << c << endl;
-		cout << "Сотни: " << c << endl
+		if (count == 2)
+			cout << "Десятки: " << c << endl;
+		if (count == 3)
+			cout << "Сотни: " << c << endl;
+		if (count == 4)
 			cout << "Тысячи: " << c << endl;
-		if (count = 5)
-			cout << "Единицы: " << c << endl;
-		cout << "Десятки: " << c << endl;
-		cout << "Сотни: " << c << endl
-			cout << "Тысячи: " << c << endl;
-		cout << "Десятки тысяч: " << c << endl;
-		if (count = 5)
-			cout << "Единицы: " << c << endl;
-		cout << "Десятки: " << c << endl;
-		cout << "Сотни: " << c << endl
-			cout << "Тысячи: " << c << endl;
-		cout << "Десятки тысяч: " << c << endl;
-		cout << "Сотни тысяч: " << c << endl;
+		if (count == 5)
+			cout << "Десятки тысяч: " << c << endl;
+		if (count == 6)
+			cout << "Сотни тысяч: " << c << endl;
+		if (count == 7)
+			cout << "Миллионы: " << c << endl;
+		if (count == 8)
+			cout << "Десятки миллионов: " << c << endl;
+		if (count == 9)
+			cout << "Сотни миллионов: " << c << endl;
+		if (count == 10)
+			cout << "Миллиарды: " << c << endl;
 	}
 	return 0;
 }
